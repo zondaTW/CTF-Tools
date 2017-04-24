642db32eaef20f153b69ae7a96619908ba0c5e16
zonda yang <u226699@gmail.com>
2017-04-24 16:11:18 +0800

    Update pcap_usb.py

--- forensics/pcap_usb/pcap_usb.py
+++ forensics/pcap_usb/pcap_usb.py
@@ -16,6 +16,7 @@ except:
     exit()
 
 dict_usb_code = usb_code.dict_usb_code
+control_list = usb_code.control_list
 keyboard_range = usb_code.keyboard_range
 keypad_range = usb_code.keypad_range
 
@@ -26,31 +27,31 @@ caps_lock = 0
 
 for line in file_content:
     line_list = line.split(':')
-    control_code = line_list[0]
+    control_code = bin(int('0x'+line_list[0], 16))[2:].rjust(8, '0')
     key_code = line_list[2]
+    control_value = ''
     value = ''
-    if key_code == '00':
-        continue
-    if key_code in keyboard_range:
-        shift = 0
-        if control_code == '20':
-            shift = 1
-        
-        value = dict_usb_code[key_code][shift]
-        
-        if caps_lock and value in string.letters:
-            value = (value.upper() if value in string.lowercase else value.lower())
-        if value == "<Enter>":
-            value += "\n"
-        if value == "<Caps Lock>":
-            caps_lock ^= 1
+    
+    shift = 0
+    for idx, bin_num in enumerate(reversed(control_code)):
+        if bin_num:
+            control_value += control_list[idx]
+            if control_list[idx] == "<Shift>":
+                shift = 1
+    
+    if not key_code == '00':
+        if key_code in keyboard_range:
+            value += dict_usb_code[key_code][shift]
+            
+            if caps_lock and value in string.letters:
+                value = (value.upper() if value in string.lowercase else value.lower())
+            if value == "<Caps Lock>":
+                caps_lock ^= 1
 
-    elif key_code in keypad_range:
-        value = dict_usb_code[key_code][num_lock]
-        if value == "<Num Lock>" or value == "<Clear>":
-            num_lock ^= 1
-    trans_content += value
+        elif key_code in keypad_range:
+            value = dict_usb_code[key_code][num_lock]
+            if value == "<Num Lock>" or value == "<Clear>":
+                num_lock ^= 1
+    trans_content += control_value + value
 
-
-
-print trans_content
+print(trans_content)
